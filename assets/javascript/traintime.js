c51b7fb7198ebf803ada0ea35eb9572e12e6cc83
ant4u <anthonyimontalto@gmail.com>
2018-11-08 17:07:40 -0500

    layout

---
+++ assets/javascript/traintime.js
@@ -0,0 +1,11 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAI43oKw7QcPXCiqmKnKRIeceWaqD94qsY",
+    authDomain: "traintime-cda5b.firebaseapp.com",
+    databaseURL: "https://traintime-cda5b.firebaseio.com",
+    projectId: "traintime-cda5b",
+    storageBucket: "traintime-cda5b.appspot.com",
+    messagingSenderId: "827108550992"
+  };
+
+  
