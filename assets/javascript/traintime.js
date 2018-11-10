11825f93923399458cb0a31eb5e0e86923bf1d56
ant4u <anthonyimontalto@gmail.com>
2018-11-09 23:58:52 -0500

    google sign-out

--- assets/javascript/traintime.js
+++ assets/javascript/traintime.js
@@ -13,12 +13,35 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 const database = firebase.database();
 
-// google Authentication
-var provider = new firebase.auth.GoogleAuthProvider();
+// google client id: 827108550992-ubcto1rqv5hjigafktd7pccr078ev6bk.apps.googleusercontent.com
+// google client secret: S0QlDBvhyr2jlCRU9YkFqF2S
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
 
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+};
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
 
 $(document).ready(function() {
 
+  
+  // google Authentication
+  var provider = new firebase.auth.GoogleAuthProvider();
+  
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
     var token = result.credential.accessToken;
