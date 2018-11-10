bfb9066ce6e9ce0dd043c2675c2e754d0bb24fc8
ant4u <anthonyimontalto@gmail.com>
2018-11-09 23:36:34 -0500

    google sign-in

--- assets/javascript/traintime.js
+++ assets/javascript/traintime.js
@@ -13,7 +13,33 @@ firebase.initializeApp(config);
 // Create a variable to reference the database.
 const database = firebase.database();
 
+// google Authentication
+var provider = new firebase.auth.GoogleAuthProvider();
+
+
 $(document).ready(function() {
+
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+  
+
+
+
   // click event for submit button
   $("#submit").on("click", function() {
     event.preventDefault();
